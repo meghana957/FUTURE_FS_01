d06081146e2ea989b154b76c45cb7a42b1909198
Meghana Reddy <meghana957@gmail.com>
2026-06-25 10:38:37 +0530

    Completed Task 1 Portfolio Website

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import "./App.css";
+
+function App() {
+return (
+<div className="container">
+
+<h1>Meghana Reddy</h1>
+
+<div className="section">
+<h2>About Me</h2>
+
+<p>
+Final Year CSE Student passionate about Full Stack Development.
+</p>
+
+</div>
+
+<div className="section">
+<h2>Skills</h2>
+
+<ul>
+<li>HTML</li>
+<li>CSS</li>
+<li>JavaScript</li>
+<li>React</li>
+<li>Python</li>
+</ul>
+
+</div>
+
+<div className="section">
+
+<h2>Projects</h2>
+
+<p>AI Meeting Cost Intelligence</p>
+
+</div>
+
+<div className="section">
+
+<h2>Contact</h2>
+
+<p>Email: yourmail@gmail.com</p>
+
+</div>
+
+</div>
+);
+}
+
+export default App;
